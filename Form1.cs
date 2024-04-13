7964c58dfb0268cd3db22114c8b56d6d56779b68
Omar Khallaf <ok7831495@gmail.com>
2024-04-13 03:40:09 +0200

    Draw Some

--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,17 @@ namespace Drawing
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            Color Red = Color.Red;
 
+            Pen pen = new Pen(Red);
+            pen.Width = 10;
+
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            e.Graphics.DrawLine(pen,100,100,100,300);
+            e.Graphics.DrawRectangle(pen, 200, 200, 300, 300);
+            e.Graphics.DrawEllipse(pen, 200, 50, 100, 120);
         }
     }
 }
